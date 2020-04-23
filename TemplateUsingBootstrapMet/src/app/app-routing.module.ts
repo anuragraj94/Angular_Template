4cbd2d4763c894e2d4a2afaacda9959fe9c13732
Anurag <anuragraj94@gmail.com>
2020-04-23 15:02:03 +0530

    Project added

---
+++ TemplateUsingBootstrapMet/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NotificationComponent } from './Components/notification/notification.component';
+import { IconsComponent } from './Components/icons/icons.component';
+import { TypoComponent } from './Components/typo/typo.component';
+import { TableListComponent } from './Components/table-list/table-list.component';
+import { UserProfileComponent } from './Components/user-profile/user-profile.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path:'', pathMatch:'full',redirectTo:'dashboard'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'userprofile',component:UserProfileComponent},
+  {path:'table',component:TableListComponent},
+  {path:'typo',component:TypoComponent},
+  {path:'icon',component:IconsComponent},
+  {path:'notification',component:NotificationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ TemplateUsingBootstrapMet/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { UserProfileComponent } from './Components/user-profile/user-profile.component';
+import { TableListComponent } from './Components/table-list/table-list.component';
+import { TypoComponent } from './Components/typo/typo.component';
+import { IconsComponent } from './Components/icons/icons.component';
+import { NotificationComponent } from './Components/notification/notification.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    UserProfileComponent,
+    TableListComponent,
+    TypoComponent,
+    IconsComponent,
+    NotificationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
